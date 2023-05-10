a2b31d1f06270e0a5d0d9e08b408b6073ed948fc
Shubham <shubhamjoshi281@gmail.com>
2023-05-10 17:47:12 +0530

    sever configure

---
+++ db/connectDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MANGO_LOCAL_URL);
+    console.log(`Data Base Connected Successfully...`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+export default connectDB;
